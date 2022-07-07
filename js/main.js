aa9cb186c1f5c136b22cc5cefc6a0c3c86203b4a
pawel <pawel.2020.navtop@gmail.com>
2022-07-07 22:21:56 +0300

    7JULE_23

--- js/main.js
+++ js/main.js
@@ -11,45 +11,50 @@ let block_number_1 = document.querySelectorAll(".block_number_conteiner_block_nu
 let block_number_2 = document.querySelectorAll(".block_number_conteiner_block_number")[2];
 
 // let block_text = document.querySelectorAll(".TheChillClubRules_block_text");
-// let block_text_0 = document.querySelectorAll(".TheChillClubRules_block_text")[0];
-// let block_text_1 = document.querySelectorAll(".TheChillClubRules_block_text")[1];
-// let block_text_2 = document.querySelectorAll(".TheChillClubRules_block_text")[2];
+let block_text_0 = document.querySelectorAll(".TheChillClubRules_block_text")[0];
+let block_text_1 = document.querySelectorAll(".TheChillClubRules_block_text")[1];
+let block_text_2 = document.querySelectorAll(".TheChillClubRules_block_text")[2];
 
 block_number_0.addEventListener("mouseover", e => {
 
-    // block_text_0.classList.add('active');
-    // block_text_1.classList.remove('active');
-    // block_text_2.classList.remove('active');
+    block_text_0.classList.remove('active');
+    block_text_1.classList.remove('active');
+    block_text_2.classList.remove('active');
 
-    block_number_0.style.width = '26%';
+    block_number_0.style.width = '56%';
     block_number_1.style.width = '13%';
     block_number_2.style.width = '13%';
 
 })
 block_number_0.addEventListener("mouseout", e => {
     block_number_0.style.width = '13%';
+    block_text_0.classList.add('active');
 });
 
 block_number_1.addEventListener("mouseover", e => {
-    // block_text_0.classList.remove('active');
-    // block_text_1.classList.add('active');
-    // block_text_2.classList.remove('active');
+    block_text_0.classList.remove('active');
+    block_text_1.classList.remove('active');
+    block_text_2.classList.remove('active');
+
     block_number_0.style.width = '13%';
-    block_number_1.style.width = '26%';
+    block_number_1.style.width = '56%';
     block_number_2.style.width = '13%';
 })
 block_number_1.addEventListener("mouseout", e => {
     block_number_1.style.width = '13%';
+    block_text_0.classList.add('active');
 });
 
 block_number_2.addEventListener("mouseover", e => {
-    // block_text_0.classList.remove('active');
-    // block_text_1.classList.remove('active');
-    // block_text_2.classList.add('active');
+    block_text_0.classList.remove('active');
+    block_text_1.classList.remove('active');
+    block_text_2.classList.remove('active');
+
     block_number_0.style.width = '13%';
     block_number_1.style.width = '13%';
-    block_number_2.style.width = '26%';
+    block_number_2.style.width = '56%';
 })
 block_number_2.addEventListener("mouseout", e => {
     block_number_2.style.width = '13%';
+    block_text_0.classList.add('active');
 })
