4df0fbbff30916c04fc76ae5b8a25070185343a6
pawel <pawel.2020.navtop@gmail.com>
2022-07-07 15:22:46 +0300

    7JULE_18

--- js/main.js
+++ js/main.js
@@ -5,4 +5,11 @@ window.onload = function () {
         document.body.classList.remove('loaded_hiding');
     }, 500)
 }
-    
+
+let block_number = document.querySelectorAll(".block_number_conteiner_block_number");
+
+block_number.forEach(element => {
+    element.addEventListener("mouseover", e => {
+        console.log(element)
+    })
+});
