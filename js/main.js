aab123821b2469646d9728f390f402b3442d043a
PawelPROGR <pawel.2020.navtop@gmail.com>
2022-07-15 12:21:19 +0300

    15JULE_3

--- js/main.js
+++ js/main.js
@@ -10,6 +10,14 @@ let block_number_0 = document.querySelectorAll(".block_number_conteiner_block_nu
 let block_number_1 = document.querySelectorAll(".block_number_conteiner_block_number")[1];
 let block_number_2 = document.querySelectorAll(".block_number_conteiner_block_number")[2];
 
+let block_number_0_textNum = document.querySelectorAll(".numBlockNm")[0];
+let block_number_1_textNum = document.querySelectorAll(".numBlockNm")[1];
+let block_number_2_textNum = document.querySelectorAll(".numBlockNm")[2];
+
+let textBlockNumber_0 = document.querySelectorAll(".textBlockNumber")[0];
+let textBlockNumber_1 = document.querySelectorAll(".textBlockNumber")[1];
+let textBlockNumber_2 = document.querySelectorAll(".textBlockNumber")[2];
+
 let block_text = document.querySelector(".TheChillClubRules_block_text");
 // let block_text_0 = document.querySelectorAll(".TheChillClubRules_block_text");
 // let block_text_1 = document.querySelectorAll(".TheChillClubRules_block_text")[1];
@@ -18,6 +26,7 @@ let block_text = document.querySelector(".TheChillClubRules_block_text");
 block_number_0.addEventListener("mouseover", e => {
 
     block_text.classList.remove('active');
+    textBlockNumber_0.style.opacity = '1'
 
     // block_text_0.classList.add('active');
     // block_text_1.classList.remove('active');
@@ -33,6 +42,8 @@ block_number_0.addEventListener("mouseout", e => {
     block_number_1.style.width = '169px';
     block_number_2.style.width = '169px';
     block_text.classList.add('active');
+    textBlockNumber_0.style.opacity = '0'
+
 });
 
 block_number_1.addEventListener("mouseover", e => {
@@ -40,6 +51,8 @@ block_number_1.addEventListener("mouseover", e => {
     // block_text_1.classList.add('active');
     // block_text_2.classList.remove('active');
     block_text.classList.remove('active');
+    textBlockNumber_1.style.opacity = '1'
+
 
     block_number_0.style.width = '0px';
     block_number_0.style.minWidth = '0px';
@@ -53,6 +66,8 @@ block_number_1.addEventListener("mouseout", e => {
     block_number_2.style.width = '169px';
     block_number_0.style.minWidth = '169px';
     block_text.classList.add('active');
+    textBlockNumber_1.style.opacity = '0'
+
 
     // block_text_0.classList.add('active');
     // block_text_1.classList.remove('active');
@@ -65,6 +80,8 @@ block_number_2.addEventListener("mouseover", e => {
     block_text.classList.remove('active');
     block_number_1.style.minWidth = '0px';
     block_number_0.style.minWidth = '0px';
+    textBlockNumber_2.style.opacity = '1'
+
 
 
     block_number_0.style.width = '0px';
@@ -78,6 +95,7 @@ block_number_2.addEventListener("mouseout", e => {
     block_number_1.style.minWidth = '169px';
     block_number_0.style.minWidth = '169px';
     block_text.classList.add('active');
+    textBlockNumber_2.style.opacity = '0'
 
     // block_text_0.classList.add('active');
     // block_text_2.classList.remove('active');
