b42047b6c23c7fbaa8282d523a3d87c13c03ce57
PawelPROGR <pawel.2020.navtop@gmail.com>
2022-07-15 14:07:00 +0300

    15JULE_14

--- js/main.js
+++ js/main.js
@@ -188,3 +188,13 @@ for (let i = 0; i <= 7; i++) {
     })
 }
 
+
+
+$(document).bind( 'mousewheel', function (e) { 
+    var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100); 
+    e.preventDefault(); 
+    e.stopPropagation(); 
+    $(document.body).stop().animate( { 
+         scrollTop : nt 
+     } , 500 , 'easeInOutCubic' );  
+} )
