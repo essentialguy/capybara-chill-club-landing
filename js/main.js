02ab08b0a4cc2568d1a470ab737dbcf59ed55952
PawelPROGR <pawel.2020.navtop@gmail.com>
2022-07-20 10:12:45 +0300

    20JULE_3

--- js/main.js
+++ js/main.js
@@ -140,7 +140,7 @@ let MintNow = document.querySelector(".MintNow");
 //можно сделать другой фон которые при прокрутке будет моментально исчезать и его высоты будет равна высоте экрана, а фон который будет прокурчиваться оставить либо все как есть, либо сделать его postiion relative 
 
 window.addEventListener("scroll", e => {
-    if (window.scrollY >= $(window).height() + 100) {
+    if (window.scrollY >= $(window).height() + 200) {
         if (background_Vector.style.top = '0vh') {
             background_Vector.style.top = '100vh';
             background_Vector.style.position = 'absolute';
